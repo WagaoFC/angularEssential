902fa905edddd672559966a331369f693073c440
Wagner Costa <wagner.costa@bit.com.br>
2023-05-09 15:05:44 -0300

    :sparkles: feat: created component product-delete

---
+++ crud/frontend/src/app/components/product/product-delete/product-delete.component.ts
@@ -0,0 +1,43 @@
+import { Component, OnInit } from '@angular/core';
+import { ProductService } from '../product.service';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Product } from '../product.model';
+
+@Component({
+  selector: 'app-product-delete',
+  templateUrl: './product-delete.component.html',
+  styleUrls: ['./product-delete.component.css']
+})
+export class ProductDeleteComponent implements OnInit {
+  constructor(
+    private productService: ProductService,
+    private router: Router,
+    private route: ActivatedRoute
+  ) {
+    this.product = {
+      id: 0,
+      name: '',
+      price: 0
+    }
+  }
+
+  product: Product
+  id = this.route.snapshot.paramMap.get('id')
+
+  ngOnInit(): void {
+    this.productService.readById(this.id!).subscribe(product => {
+      this.product = product
+    })
+  }
+
+  deleteProduct(): void {
+    this.productService.delete(this.id!).subscribe(() => {
+      this.productService.showMessage("Produto deletado com sucesso")
+      this.router.navigate(['/products'])
+    })
+  }
+
+  cancel(): void {
+    this.router.navigate(['/products'])
+  }
+}
